fa36211466a98ba07b74ef3eebf23e7db0fa59c8
Farhod Ganijonov <farhod089@gmail.com>
2026-01-22 10:49:17 +0500

    login url successfull

--- specialist/admin.py
+++ specialist/admin.py
@@ -1,5 +1,6 @@
 from admin_auto_filters.filters import AutocompleteFilter
 from django.contrib import admin
+from django import forms
 from modeltranslation.admin import TabbedTranslationAdmin
 from .models import Doctor, TypeDoctor, RateDoctor, AdviceTime, Advertising
 
@@ -23,12 +24,49 @@ class TypeDoctorAdmin(TabbedTranslationAdmin):
     list_display = ('id', 'name', 'image', )
     search_fields = ['id', 'name', ]
 
+class DoctorAdminForm(forms.ModelForm):
+    phone = forms.CharField(max_length=20, disabled=True)
+    is_approved = forms.BooleanField(required=False)
+
+    class Meta:
+        model = Doctor
+        exclude = ('user',)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        if self.instance.pk and self.instance.user:
+            self.fields['phone'].initial = self.instance.user.phone
+            self.fields['is_approved'].initial = self.instance.user.is_approved
+
 
 @admin.register(Doctor)
 class DoctorAdmin(admin.ModelAdmin):
-    list_display = ('full_name', 'type_doctor', 'is_verified')
-    readonly_fields = ('created_at', 'user')
+    form = DoctorAdminForm
+
+    list_display = (
+        'full_name',
+        'type_doctor',
+        'is_verified',
+        'user_is_approved',
+    )
+
+    list_filter = ('is_verified',)
     search_fields = ('full_name', 'user__phone')
+    readonly_fields = ('created_at',)
+
+    def user_is_approved(self, obj):
+        return obj.user.is_approved
+    user_is_approved.boolean = True
+    user_is_approved.short_description = "Approved"
+
+    def save_model(self, request, obj, form, change):
+        super().save_model(request, obj, form, change)
+
+        if obj.user:
+            obj.user.is_approved = form.cleaned_data.get('is_approved', False)
+            obj.user.is_active = obj.user.is_approved
+            obj.user.save()
 
 
 class RateDoctorAdmin(admin.ModelAdmin):
