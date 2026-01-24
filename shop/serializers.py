ee018576fcb088439481cb20f77b8ff1dc57302e
Farhod Ganijonov <farhod089@gmail.com>
2026-01-24 12:58:53 +0500

    edit edicine uz ru en 2

--- shop/serializers.py
+++ shop/serializers.py
@@ -28,7 +28,7 @@ class MedicineSerializer(serializers.ModelSerializer):
         model = Medicine
 
         fields = [
-            'id', 'image', 'name', 'name_uz' 'name_ru', 'name_en', 'title', 'title_uz', 'title_ru', 'title_en',
+            'id', 'image', 'name', 'name_uz', 'name_ru', 'name_en', 'title', 'title_uz', 'title_ru', 'title_en',
             'order_count','description', 'description_uz', 'description_ru', 'description_ru', 'quantity', 'review',
             'weight', 'type_medicine', 'cost', 'discount', 'total_rate', 'is_favorite', 'pictures',
         ]
@@ -66,7 +66,7 @@ class MedicineDetailSerializer(MedicineSerializer):
     class Meta:
         model = Medicine
         fields = [
-            'id', 'image', 'name', 'name_uz' 'name_ru', 'name_en', 'title', 'title_uz', 'title_ru', 'title_en',
+            'id', 'image', 'name', 'name_uz', 'name_ru', 'name_en', 'title', 'title_uz', 'title_ru', 'title_en',
             'order_count', 'description', 'description_uz', 'description_ru', 'description_ru',
             'quantity', 'review', 'weight', 'type_medicine', 'cost', 'discount',
             'created_at', 'product_inn', 'product_ikpu', 'product_package_code',
