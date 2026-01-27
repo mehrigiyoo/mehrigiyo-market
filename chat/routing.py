8b462b0c0f5bc280443d045011f6da90ace43538
Farhod Ganijonov <farhod089@gmail.com>
2026-01-27 13:26:27 +0500

    edit middleware file2

--- chat/routing.py
+++ chat/routing.py
@@ -1,9 +1,8 @@
 from django.urls import re_path
-
-from . import consumers
+from .consumers import ChatConsumer
 
 websocket_urlpatterns = [
-    re_path(r'chat/(?P<room_id>\w+)/$', consumers.ChatConsumer.as_asgi()),
+    re_path(r"ws/chat/(?P<room_id>\w+)/$", ChatConsumer.as_asgi()),
     # re_path(r'videochat/(?P<room_id>\w+)/$', consumers.VideoChatConsumer.as_asgi()),
     # re_path(r'events/$', consumers.EventsConsumer.as_asgi()),
 ]
