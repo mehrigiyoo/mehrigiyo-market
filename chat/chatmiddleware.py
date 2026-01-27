4be49e40fca03e752fa32be4d00367a20d93f051
Farhod Ganijonov <farhod089@gmail.com>
2026-01-27 13:16:11 +0500

    edit middleware file

--- chat/chatmiddleware.py
+++ chat/chatmiddleware.py
@@ -1,38 +1,45 @@
-from channels.auth import AuthMiddlewareStack
 from channels.db import database_sync_to_async
-from django.contrib.auth.models import AnonymousUser
 from rest_framework_simplejwt.tokens import UntypedToken
 from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
 from jwt import decode as jwt_decode
 from django.conf import settings
+from django.contrib.auth.models import AnonymousUser
 from account.models import UserModel
-
 
 @database_sync_to_async
 def get_user(token_key):
     try:
         UntypedToken(token_key)
-    except (InvalidToken, TokenError) as e:
-        return None
-    else:
-        decoded_data = jwt_decode(token_key, settings.SECRET_KEY, algorithms=["HS256"])
+    except (InvalidToken, TokenError):
+        return AnonymousUser()
+    decoded_data = jwt_decode(token_key, settings.SECRET_KEY, algorithms=["HS256"])
+    try:
         user = UserModel.objects.get(id=decoded_data["user_id"])
+    except UserModel.DoesNotExist:
+        user = AnonymousUser()
     return user
 
-from urllib.parse import parse_qs
 class TokenAuthMiddleware:
+    """
+    Custom middleware for Channels 3
+    Token JWT auth via headers: Authorization: Bearer <token>
+    """
+
+    def __init__(self, inner):
+        self.inner = inner
+
     async def __call__(self, scope, receive, send):
+        # headers keys are bytes
         headers = dict(scope['headers'])
-        # headers keys are bytes, values are bytes
         auth_header = headers.get(b'authorization', None)
         token_key = None
         if auth_header:
-            # b'Bearer <token>'
             token_key = auth_header.decode().split(' ')[1]
 
-        if not token_key:
-            scope['user'] = AnonymousUser()
-        else:
-            scope['user'] = await get_user(token_key)
+        scope['user'] = await get_user(token_key) if token_key else AnonymousUser()
 
         return await self.inner(scope, receive, send)
+
+
+# Helper for stacking with AuthMiddlewareStack if needed
+TokenAuthMiddlewareStack = lambda inner: TokenAuthMiddleware(inner)
--- config/asgi.py
+++ config/asgi.py
@@ -10,29 +10,23 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 import os
 import django
 from django.core.asgi import get_asgi_application
-from django.urls import re_path
+from channels.routing import ProtocolTypeRouter, URLRouter
+from chat.chatmiddleware import TokenAuthMiddlewareStack
+from chat.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 django_asgi_app = get_asgi_application()
 django.setup()
 
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-from chat.chatmiddleware import TokenAuthMiddleware
-from chat.routing import websocket_urlpatterns
-
-
-
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
-    "websocket": TokenAuthMiddleware(
-        AuthMiddlewareStack(
-            URLRouter([
-                re_path(r"^ws/", URLRouter(websocket_urlpatterns)),
-            ])
+    "websocket": TokenAuthMiddlewareStack(
+        URLRouter(
+            websocket_urlpatterns
         )
     ),
 })
+
 
 # application = ProtocolTypeRouter(
 #     {
